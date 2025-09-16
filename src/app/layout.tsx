a6fe219c5d3c0e60a7435c2819253a45f08bd530
Jonathan Kerth <Jonathanpkerth@gmail.com>
2025-09-15 18:03:19 -0700

    remove unused google auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -74,10 +74,6 @@ export default function RootLayout({
 			<head>
 				<link rel="icon" href="/favicon.ico" sizes="any" />
 				<meta name="google-adsense-account" content="ca-pub-1895631836444724" />
-				<meta
-					name="google-site-verification"
-					content="your-verification-code"
-				/>
 				<link rel="canonical" href="https://www.thenamenursery.com" />
 				<link
 					rel="preconnect"
